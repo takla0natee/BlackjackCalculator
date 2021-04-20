d96adeff44daaea8f86a49fd72d2594fe3ca55c3
takla0natee <37974342+takla0natee@users.noreply.github.com>
2021-04-20 13:37:13 +0700

    next stage available

--- UIUX/flow_control.js
+++ UIUX/flow_control.js
@@ -1,5 +1,15 @@
 nextButton = document.getElementById("next-button");
 
+//Assign each state to an variable
+valueSelect = document.getElementById("value-selection");
+suiteSelect = document.getElementById("suite-selection");
+
 nextButton.addEventListener('click', function(){
     console.log('hi');
+    suiteSelect.classList.add("stage-hide");
+    suiteSelect.classList.remove("stage");
+
+    valueSelect.classList.remove("stage-hide");
+    valueSelect.classList.add("stage");
 });
+

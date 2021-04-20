4c7405b86f574024202ece96ab6b8c02608eab74
takla0natee <37974342+takla0natee@users.noreply.github.com>
2021-04-20 14:52:09 +0700

    'update more pages'

--- UIUX/flow_control.js
+++ UIUX/flow_control.js
@@ -1,15 +1,53 @@
-nextButton = document.getElementById("next-button");
+//Call elements in each stage
 
-//Assign each state to an variable
+//suite-select
+nextButtonToRank = document.getElementById("next-button-to-rank");
+//rank-select
+nextButtonToSum = document.getElementById("next-button-to-sum");
+backButtonToSuite = document.getElementById("back-button-to-suite");
+//summary page
+addCardButton = document.getElementById("add-button");
+backButtonToRank = document.getElementById("back-button-to-rank");
+
+
+
+//Assign each stage to a variable
 valueSelect = document.getElementById("value-selection");
 suiteSelect = document.getElementById("suite-selection");
+summaPage = document.getElementById("cards-summary")
 
-nextButton.addEventListener('click', function(){
-    console.log('hi');
-    suiteSelect.classList.add("stage-hide");
+//Button action
+//SuiteSelectPage
+nextButtonToRank.addEventListener('click', function(){
     suiteSelect.classList.remove("stage");
+    suiteSelect.classList.add("stage-hide");
 
     valueSelect.classList.remove("stage-hide");
     valueSelect.classList.add("stage");
 });
 
+//ValueSelectPage
+nextButtonToSum.addEventListener('click', function(){
+    valueSelect.classList.remove("stage");
+    valueSelect.classList.add("stage-hide");
+    
+    summaPage.classList.remove("stage-hide");
+    summaPage.classList.add("stage");
+});
+
+backButtonToSuite.addEventListener('click', function(){
+    suiteSelect.classList.add("stage");
+    suiteSelect.classList.remove("stage-hide");
+
+    valueSelect.classList.add("stage-hide");
+    valueSelect.classList.remove("stage");
+});
+
+//Summary Page
+addCardButton.addEventListener('click', function(){
+    suiteSelect.classList.add("stage");
+    suiteSelect.classList.remove("stage-hide");
+
+    summaPage.classList.add("stage-hide");
+    summaPage.classList.remove("stage");
+});

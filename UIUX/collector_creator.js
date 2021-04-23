c5adec673fe0498e4f4505ed25009d3a38fea35f
takla0natee <37974342+takla0natee@users.noreply.github.com>
2021-04-23 16:35:35 +0700

    Reorganise file and reroute the path to scripts

--- UIUX/collector_creator.js
+++
@@ -1,9 +0,0 @@
-import * as control from './flow_control.js';
-
-//Assign each stage to a variable
-valueSelect = document.getElementById("value-selection");
-suiteSelect = document.getElementById("suite-selection");
-summaPage = document.getElementById("cards-summary");
-
-
- 
